5589309cee920f7af0f2024dab82020932fc7a99
bilalaljbawe13-ux <bilalaljbawe13@gmail.com>
2026-08-27 01:56:32 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+
+const CACHE="4u-v7";
+const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{
+    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
+  }).catch(()=>cached)));
+});
